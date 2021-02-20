b49ab4b5bf60a99657252016890499202255550b
Peerawat Poombua <peerawat@odds.team>
2021-02-20 15:42:26 +0700

    make base url to be configurable by env

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        baseURL: 'http://localhost:8080'
+        baseURL: process.env.BASE_URL || 'http://localhost:8080'
     },
 
     // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
