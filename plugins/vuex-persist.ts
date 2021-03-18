3327d7b026a566087faf27fe34b395552147429a
Peerawat Poombua <peerawat@odds.team>
2021-03-18 23:09:45 +0700

    udpate

--- plugins/vuex-persist.ts
+++ plugins/vuex-persist.ts
@@ -1,16 +1,8 @@
 import { Context } from '@nuxt/types'
 import VuexPersistence from 'vuex-persist'
 
-declare global {
-    interface Window {
-        onNuxtReady: Function
-    }
-}
-
 export default function(context: Context) {
-    window.onNuxtReady(() => {
-        new VuexPersistence({
-            key: 'yfc'
-        }).plugin(context.store)
-    })
+    new VuexPersistence({
+        key: 'yfc'
+    }).plugin(context.store)
 }
